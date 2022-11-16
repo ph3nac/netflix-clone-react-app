b8c34c25de6bf22d47a8bc4d887411a94e78568f
ph3 <100564304+ph3nac@users.noreply.github.com>
2022-11-16 17:01:35 +0900

    add Banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import "./App.css";
 import { Row } from "./Row";
 import { requests } from "./request";
+import { Banner } from "./Banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.feachNetflixOriginals}
         isLargeRow
       />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}
-      />
-      <Row title="Anime" fetchUrl={requests.fetchAnime}/>
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-      <Row title="History Movies" fetchUrl={requests.fetchHistoryMovies}/>
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Anime" fetchUrl={requests.fetchAnime} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="History Movies" fetchUrl={requests.fetchHistoryMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
